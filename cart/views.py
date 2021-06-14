7e6b98796d04c9a695218b5999cde1111a43dbec
imhimansu28 <imhimansu28@gmail.com>
2021-06-14 23:59:39 +0530

    add to cart bug fix

--- cart/views.py
+++ cart/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import redirect, render
+from django.shortcuts import get_object_or_404, redirect, render
+from django.http import HttpResponse
 from .models import CartItem , Cart
 from store.models import Product
-from django.http import HttpResponse
+
 
 # Create your views here.
 def _cart_id(request):
@@ -31,9 +32,43 @@ def add_cart(request, product_id):
             cart = cart,
         )
         cart_item.save()
-    
-    return HttpResponse(cart_item.product)
     return redirect('cart')
 
-def cart(request):
-    return render(request, 'store/cart.html')
+def remove_cart(request, product_id):
+    cart = Cart.objects.get(cart_id = _cart_id(request))
+    product = get_object_or_404(Product, id = product_id)
+    cart_item = CartItem.objects.get(product=product, cart = cart)
+    if cart_item.quantity > 1:
+        cart_item.quantity -= 1
+        cart_item.save()
+    else:
+        cart_item.delete()
+    return redirect('cart')
+
+def remove_cart_item(request, product_id):
+    cart = Cart.objects.get(cart_id = _cart_id(request))
+    product = get_object_or_404(Product, id = product_id)
+    cart_item = CartItem.objects.get(product=product, cart = cart)
+    cart_item.delete()
+    return redirect('cart')
+
+
+def cart(request, total = 0, quantity = 0, cart_items= None ):
+    try:
+        cart = Cart.objects.get(cart_id = _cart_id(request))
+        cart_items = CartItem.objects.filter(cart=cart, is_active = True)
+        for cart_item in cart_items:
+            total += (cart_item.product.price * cart_item.quantity)
+            quantity += cart_item.quantity
+        tax = (2*total)/100
+        grand_total = total+tax
+    except ObjectNotExist:
+        pass
+    context = {
+        'total':total,
+        'quantity':quantity,
+        'cart_items': cart_items,
+        'tax' : tax,
+        'grand_total':grand_total,
+    }
+    return render(request, 'store/cart.html', context)
