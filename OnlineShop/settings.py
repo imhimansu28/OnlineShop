bd5ee458fc22a83b62da12b53868e2a4e0b330e7
Himanshu Gupta <57537963+imhimansu28@users.noreply.github.com>
2021-09-23 23:23:14 +0530

    Update settings.py

--- OnlineShop/settings.py
+++ OnlineShop/settings.py
@@ -146,5 +146,5 @@ MESSAGE_TAGS = {
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_POST = 587
 EMAIL_HOST_USER = 'webdevhimanshu@gmail.com'
-EMAIL_HOST_PASSWORD = 'lpncxuayncdzpqoo'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
